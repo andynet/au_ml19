40be18949e143352b36784a889e5392e50fda41e
andynet <andrejbalaz001@gmail.com>
2019-09-25 12:46:59 +0200

    logistic regression finished

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,20 +1,21 @@
 import numpy as np
 from h1_util import numerical_grad_check
 
+
 def logistic(z):
-    """ 
+    """
     Helper function
     Computes the logistic function 1/(1+e^{-x}) to each entry in input vector z.
-    
+
     np.exp may come in handy
     Args:
-        z: numpy array shape (d,) 
+        z: numpy array shape (d,)
     Returns:
-       logi: numpy array shape (d,) each entry transformed by the logistic function 
+       logi: numpy array shape (d,) each entry transformed by the logistic function
     """
     logi = np.zeros(z.shape)
     ### YOUR CODE HERE 1-5 lines
-    
+    logi = 1 / (1 + np.exp(-z))
     ### END CODE
     assert logi.shape == z.shape
     return logi
@@ -27,114 +28,138 @@ class LogisticRegressionClassifier():
 
     def cost_grad(self, X, y, w):
         """
-        Compute the average negative log likelihood and gradient under the logistic regression model 
-        using data X, targets y, weight vector w 
-        
+        Compute the average negative log likelihood and gradient under the logistic regression model
+        using data X, targets y, weight vector w
+
         np.log, np.sum, np.choose, np.dot may be useful here
         Args:
-           X: np.array shape (n,d) float - Features 
-           y: np.array shape (n,)  int - Labels 
+           X: np.array shape (n,d) float - Features
+           y: np.array shape (n,)  int - Labels
            w: np.array shape (d,)  float - Initial parameter vector
 
         Returns:
-           cost: scalar: the average negative log likelihood for logistic regression with data X, y 
-           grad: np.arrray shape(d, ) gradient of the average negative log likelihood at w 
+           cost: scalar: the average negative log likelihood for logistic regression with data X, y
+           grad: np.arrray shape(d, ) gradient of the average negative log likelihood at w
         """
         cost = 0
         grad = np.zeros(w.shape)
         ### YOUR CODE HERE 5 - 15 lines
+        y = y.reshape(-1, 1)
+        w = w.reshape(-1, 1)
+
+        nll = -np.sum(y.T @ np.log(logistic(X @ w))
+                      + (1 - y.T) @ np.log(1 - logistic(X @ w)))    # NLL(X|w)
+        cost = nll / X.shape[0]
+
+        grad_nll = -(X.T @ (y - logistic(X @ w)))                   # grad NLL(X|w)
+        grad = grad_nll / X.shape[0]
         ### END CODE
         assert grad.shape == w.shape
         return cost, grad
 
-
     def fit(self, X, y, w=None, lr=0.1, batch_size=16, epochs=10):
         """
-        Run mini-batch stochastic Gradient Descent for logistic regression 
+        Run mini-batch stochastic Gradient Descent for logistic regression
         use batch_size data points to compute gradient in each step.
-    
+
         The function np.random.permutation may prove useful for shuffling the data before each epoch
         It is wise to print the performance of your algorithm at least after every epoch to see if progress is being made.
         Remeber the stochastic nature of the algorithm may give fluctuations in the cost as iterations increase.
 
         Args:
-           X: np.array shape (n,d) dtype float32 - Features 
-           y: np.array shape (n,) dtype int32 - Labels 
+           X: np.array shape (n,d) dtype float32 - Features
+           y: np.array shape (n,) dtype int32 - Labels
            w: np.array shape (d,) dtype float32 - Initial parameter vector
            lr: scalar - learning rate for gradient descent
            batch_size: number of elements to use in minibatch
            epochs: Number of scans through the data
 
-        sets: 
+        sets:
            w: numpy array shape (d,) learned weight vector w
            history: list/np.array len epochs - value of cost function after every epoch. You know for plotting
         """
-        if w is None: w = np.zeros(X.shape[1])
+        if w is None:
+            w = np.zeros(X.shape[1])
         history = []        
         ### YOUR CODE HERE 14 - 20 lines
+        for i in range(epochs):
+            indices = np.random.permutation(range(X.shape[0]))
+            cost_total = 0
+            for j in range(0, X.shape[0], batch_size):
+                X_batch = X[indices[j: j + batch_size], :]
+                y_batch = y[indices[j: j + batch_size]]
+
+                cost, grad = self.cost_grad(X_batch, y_batch, w)
+                grad = grad.ravel()
+                w -= (lr * grad)
+                cost_total += cost * X_batch.shape[0]
+
+            cost = cost_total / X.shape[0]
+            history.append(cost)
+            self.w = w
         ### END CODE
         self.w = w
         self.history = history
-
 
     def predict(self, X):
         """ Classify each data element in X
 
         Args:
-            X: np.array shape (n,d) dtype float - Features 
-        
-        Returns: 
+            X: np.array shape (n,d) dtype float - Features
+
+        Returns:
            p: numpy array shape (n, ) dtype int32, class predictions on X (0, 1)
 
         """
         out = np.zeros(X.shape[0])
         ### YOUR CODE HERE 1 - 4 lines
+        out = logistic(X @ self.w)
         ### END CODE
         return out
-    
+
     def score(self, X, y):
         """ Compute model accuracy  on Data X with labels y
 
         Args:
-            X: np.array shape (n,d) dtype float - Features 
-            y: np.array shape (n,) dtype int - Labels 
+            X: np.array shape (n,d) dtype float - Features
+            y: np.array shape (n,) dtype int - Labels
 
-        Returns: 
+        Returns:
            s: float, number of correct prediction divivded by n.
 
         """
         s = 0
         ### YOUR CODE HERE 1 - 4 lines
+        s = np.mean(np.round(self.predict(X)) == y)
         ### END CODE
         return s
-        
 
-    
+
 def test_logistic():
-    print('*'*5, 'Testing logistic function')
+    print('*' * 5, 'Testing logistic function')
     a = np.array([0, 1, 2, 3])
     lg = logistic(a)
-    target = np.array([ 0.5, 0.73105858, 0.88079708, 0.95257413])
+    target = np.array([0.5, 0.73105858, 0.88079708, 0.95257413])
     assert np.allclose(lg, target), 'Logistic Mismatch Expected {0} - Got {1}'.format(target, lg)
     print('Test Success!')
 
-    
+
 def test_cost():
-    print('*'*5, 'Testing Cost Function')
+    print('*' * 5, 'Testing Cost Function')
     X = np.array([[1.0, 0.0], [1.0, 1.0], [3, 2]])
     y = np.array([0, 0, 1], dtype='int64')
     w = np.array([0.0, 0.0])
     print('shapes', X.shape, w.shape, y.shape)
     lr = LogisticRegressionClassifier()
-    cost,_ = lr.cost_grad(X, y, w)
+    cost, _ = lr.cost_grad(X, y, w)
     target = -np.log(0.5)
     assert np.allclose(cost, target), 'Cost Function Error:  Expected {0} - Got {1}'.format(target, cost)
     print('Test Success')
 
-    
+
 def test_grad():
-    print('*'*5, 'Testing  Gradient')
-    X = np.array([[1.0, 0.0], [1.0, 1.0], [2.0, 3.0]])    
+    print('*' * 5, 'Testing  Gradient')
+    X = np.array([[1.0, 0.0], [1.0, 1.0], [2.0, 3.0]])
     w = np.array([0.0, 0.0])
     y = np.array([0, 0, 1]).astype('int64')
     print('shapes', X.shape, w.shape, y.shape)
@@ -144,10 +169,7 @@ def test_grad():
     print('Test Success')
 
 
-    
 if __name__ == '__main__':
     test_logistic()
     test_cost()
     test_grad()
-    
-    
